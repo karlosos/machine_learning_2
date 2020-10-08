f95fe3934bdb3cff026c54aaf7981879752f36a9
Karol Dzialowski <karlososhd@gmail.com>
2020-10-08 11:05:07 +0200

    feat: haar templates

--- src/haar.py
+++ src/haar.py
@@ -2,6 +2,16 @@ import numpy as np
 import cv2
 
 DEF_HEIGHT = 480
+ 
+# each template defined via a list of white rectangles located within a unit square
+# each rectangle given as a quadruple (j, k), (h, w)
+HAAR_TEMPLATES = [
+    np.array([0.0, 0.0, 1.0, 0.5]), # left-right edge
+    np.array([0.0, 0.0, 0.5, 1.0]), # up-down edge
+    np.array([0.0, 0.25, 1.0, 0.5]), # left-middle-right edge
+    np.array([0.25, 0, 0.5, 1]), # left-middle-right edge
+    np.array([[0.0, 0.0, 0.5, 0.5], [0.5, 0.5, 0.5, 0.5]]) # diagonal
+]
 
 def resize_image(i):
     width = int(np.round(i.shape[1] * DEF_HEIGHT / (1.0 * i.shape[0])))
